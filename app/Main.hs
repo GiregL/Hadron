9a77a5036a0540b7c27af5d1d1c5ef5b974ad6b7
Lezenn <gireg.lezoraine@gmail.com>
2019-10-28 00:04:59 +0100

    Created file parsing argument

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,21 @@
 module Main where
 
+import System.IO
+import System.Environment
+import Control.Monad
+import Text.Parsec
+
 import Lib
+import Parser
 
 main :: IO ()
 main = do
-    print "Hadron Compiler"
+    args <- getArgs
+    mapM_ parseFile args
+
+parseFile :: String -> IO ()
+parseFile path = do
+    handler <- openFile path ReadMode
+    fileContent <- hGetContents handler
+    print $ parse file path fileContent
+    hClose handler
